a0209fe2e6380e7e42e256348c9f4222aa73be5b
Rime <81419447+Emirlol@users.noreply.github.com>
2024-12-05 20:34:37 +0300

    Oops, forgot to add 1.21.4 in the build.gradle.kts as well

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ publishMods {
     modrinth {
         accessToken = providers.environmentVariable("MODRINTH_TOKEN")
         projectId = "o2oFdqXS"
-        minecraftVersions.addAll("1.20.5", "1.20.6", "1.21", "1.21.1", "1.21.2", "1.21.3")
+        minecraftVersions.addAll("1.20.5", "1.20.6", "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4")
         requires("fabric-language-kotlin")
         featured = true
     }
